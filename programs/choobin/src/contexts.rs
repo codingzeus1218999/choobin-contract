e285747bccd018f43b26987a2915b16ed31f91a9
smart0120 <mitato355@gmail.com>
2024-05-07 08:40:29 +0300

    Update smart contract

--- programs/choobin/src/contexts.rs
+++ programs/choobin/src/contexts.rs
@@ -140,6 +140,15 @@ pub struct SetEndtime<'info> {
 }
 
 #[derive(Accounts)]
+pub struct SetPrice<'info> {
+    #[account(mut, seeds = [PRESALE_INFO_SEED.as_bytes()], bump)]
+    pub presale_info: Account<'info, PresaleInfo>,
+
+    #[account(mut, constraint = admin.to_account_info().key() == presale_info.admin)]
+    pub admin: Signer<'info>,
+}
+
+#[derive(Accounts)]
 pub struct CreateUserInfo<'info> {
     #[account(init_if_needed, seeds = [USER_INFO_SEED.as_bytes(), user.key().as_ref()], bump, 
         payer = user, space = size_of::<UserInfo>() + DISCRIMINATOR_LENGTH)]
@@ -180,7 +189,7 @@ pub struct BuyToken<'info> {
     pub mint: Account<'info, Mint>,
 
     /// CHECK: This is not dangerous because we don't read or write from this account
-    #[account( constraint = treasury.key() == presale_info.treasury)]
+    #[account(mut, constraint = treasury.key() == presale_info.treasury)]
     pub treasury: AccountInfo<'info>,
 
     pub system_program: Program<'info, System>,
@@ -208,9 +217,10 @@ pub struct Claim<'info> {
     #[account(mut, constraint = mint.to_account_info().key() == presale_info.mint)]
     pub mint: Account<'info, Mint>,
 
-    #[account(mut,
+    #[account(init_if_needed,
         associated_token::mint = mint,
         associated_token::authority = user,
+        payer = user
     )]    
     pub user_mint_ata: Account<'info, TokenAccount>,
 
